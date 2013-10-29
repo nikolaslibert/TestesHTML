f12f5fd6b64f0250636c46a96869ecec6a1c2756
Nikolas Libert <nikolas@libert.com>
2013-10-29 21:05:55 -0200

    Projet

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="CSS/designPrincipal.css">
+        <title></title>
+    </head>
+    <body>
+        <!--------------------------------------------------------------------->        
+        <div id="containerCabecalho">
+            <div class="leiauteColunaPrincipal">
+                <div id="cabecalhoTitulo">
+                    <h1 id="cabecalhoTitulo">Pé Hashado</h1>
+                </div>
+                
+                <div id="cabecalhoLogin">
+                    <p>Login</p>
+                </div>
+            </div>
+        </div>
+        <!--------------------------------------------------------------------->
+        
+        <!--------------------------------------------------------------------->
+        <div id="containerConteudo">
+            <div id="conteudo">
+                <div id="conteudoEsq">
+                    <p>Login</p>
+                </div>
+                
+                <div id="conteudoDir">
+                    <p>Mapa</p>
+                </div>                
+            </div>
+        </div>
+        <!--------------------------------------------------------------------->
+        
+        <!--------------------------------------------------------------------->
+        <div id="containerRodape">
+            <div class="leiauteColunaPrincipal">
+                <p>Pé Rachado</p>
+            </div>
+        </div>
+        <!--------------------------------------------------------------------->
+        
+        
+    </body>
+</html>
